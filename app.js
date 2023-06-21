22045a65149c74eac13771d142aa084c82741ff0
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-06-21 12:22:41 +0300

    add celebrate errors в app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const router = require('./routes');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -7,5 +8,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(errors());
 app.listen(3000, () => {
 });
