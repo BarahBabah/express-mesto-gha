af43d2948066b0b44b220d81401d32d9036de5b9
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-06-22 01:02:40 +0300

    hotfix statuscode

--- utils/constants.js
+++ utils/constants.js
@@ -1,16 +1,24 @@
-const STATUS_CODES = {
-  OK: 200,
-  CREATED: 201,
-  BAD_REQUEST: 400,
-  NOT_FOUND: 404,
-  SERVER_ERROR: 500,
-  CONFLICT: 409,
-  FORBIDDEN: 403,
-  NOT_AUTHORIZED: 401,
-  DUBLICATE_KEY_ERROR: 11000,
-};
-
+const OK = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const SERVER_ERROR = 500;
+const CONFLICT = 409;
+const FORBIDDEN = 403;
+const NOT_AUTHORIZED = 401;
+const DUPLICATE_KEY_ERROR = 11000;
 // eslint-disable-next-line no-useless-escape
 const patternUrl = /http(s)?:\/\/(www.)?[a-z0-9\.\-]+\/[a-z0-9\.\-_~:\/?#\[\]@!$&'()*+,;=]+/;
 
-module.exports = { STATUS_CODES, patternUrl };
+module.exports = {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  NOT_FOUND,
+  SERVER_ERROR,
+  CONFLICT,
+  FORBIDDEN,
+  NOT_AUTHORIZED,
+  DUPLICATE_KEY_ERROR,
+  patternUrl,
+};
