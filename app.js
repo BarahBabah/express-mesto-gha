4ecab3648df6414e2c8e6a187d3c2080c6d33432
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-06-24 05:02:34 +0300

    add cors/dotenv

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1/mestodb' } = process.env;
 const app = express();
+app.use(cors());
+
+mongoose.connect(MONGO_URL);
+
+mongoose.set({ runValidators: true });
+
 app.use(express.json());
 app.use(router);
-app.listen(3000, () => {
+app.listen(PORT, () => {
 });
